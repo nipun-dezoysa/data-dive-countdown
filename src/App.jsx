50a45308af81eb843f581008fbdb4d7c21f0be5e
nipun-dezoysa <nipunavishka123@gmail.com>
2025-01-19 20:20:33 +0530

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
-
-import acds from "./assets/acds.png";
-import department from "./assets/department.png";
+import Footer from "./Footer";
 import Background from "./Background";
 function App() {
-
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center bg-black text-white">
       <div className="absolute top-0 w-full h-full">
         <Background />
       </div>
       <div className="z-30 flex flex-col items-center gap-5">
-        <div className="text-center text-7xl font-bold">TIME TO DOMINATE</div>
-        <div className="flex items-center gap-5 ">
+        <div className="text-center text-5xl font-bold">TIME TO DOMINATE</div>
+        <div className="flex items-center gap-12">
           <div className="text-center">
-            <div className="font-semibold text-2xl font-mono">Hours</div>
             <div className="text-9xl font-bold">04</div>
+            <div className="font-semibold text-2xl font-mono">Hours</div>
           </div>
           <div className="text-center">
-            <div className="font-semibold text-2xl font-mono">Minutes</div>
             <div className="text-9xl font-bold">53</div>
+            <div className="font-semibold text-2xl font-mono">Minutes</div>
           </div>
           <div className="text-center">
+            <div className="text-9xl font-bold">23</div>{" "}
             <div className="font-semibold text-2xl font-mono">Seconds</div>
-            <div className="text-9xl font-bold">23</div>
           </div>
         </div>
-        <div className="text-center text-7xl font-bold">Hurry Up</div>
       </div>
-      <div className="absolute bottom-0 flex gap-5 bg-white pt-5 px-10 rounded-t-3xl">
-        <img className="w-[120px]" src={acds} alt="" />
-        <img className="w-[120px]" src={department} alt="" />
-      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,20 @@
+import acds from "./assets/acds.png";
+import department from "./assets/department.png";
+import nsbm from "./assets/nsbm.png";
+import codebell from "./assets/codebell.png";
+import afsaan from "./assets/afsaan.png";
+import datadive from "./assets/datadive.png";
+function Footer() {
+  return (
+    <div className="absolute bottom-5 flex items-center gap-5 bg-white/10 backdrop-blur-sm py-2 px-10 rounded-3xl shadow-lg">
+      <img className="footer-img" src={nsbm} alt="" />
+      <img className="w-[120px]" src={acds} alt="" />
+      <img className="w-[120px]" src={department} alt="" />
+      <img className="w-[120px]" src={datadive} alt="" />
+      <img className="footer-img" src={codebell} alt="" />
+      <img className="footer-img" src={afsaan} alt="" />
+    </div>
+  );
+}
+
+export default Footer;
